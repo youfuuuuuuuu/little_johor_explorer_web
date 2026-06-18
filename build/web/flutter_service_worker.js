575b6d7b4012a3c88679036dd8933563f7c0a7cf
youfuuuuuuuu <tanyoufu5714@gmail.com>
2026-06-18 22:28:40 +0800

    Fixed asset paths and re-deployed

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -198,7 +198,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4ad9c1dce38a842f0d17ce1739e1bb55
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "175f846dc53943ec5638ada01c026e62",
+"flutter_bootstrap.js": "3a71add8ba9b87cdf000515d7e4b2307",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
